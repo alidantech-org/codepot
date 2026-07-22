13a63a18d7f7340a8c86664350fbdf5a053e2524
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-22 21:45:57 +0300

    feat(templating): export catalog and validation APIs

--- packages/nodejs/codepotx/src/templating/index.ts
+++ packages/nodejs/codepotx/src/templating/index.ts
@@ -1,3 +1,16 @@
+export { BUILTIN_TEMPLATE_HELPERS, createTemplateRenderer } from './helpers';
+export { buildTemplateContext, createNameSet } from './template-context';
+export {
+  collectTemplateReferences,
+  validatePathExpression,
+  validateTemplateReferences,
+} from './template-references';
+export {
+  buildTemplateVariableCatalog,
+  collectVariableEntries,
+  formatTemplateVariableCatalog,
+  validateTemplateContext,
+} from './template-variables';
 export { compilePathParts, compilePathTokens, resolveExpression, resolveOutputTokens } from './path-tokens';
 export { createTemplatingEngine, DefaultTemplatingEngine } from './templating-engine';
 export type * from './templating.types';
