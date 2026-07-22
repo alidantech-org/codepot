a4b09344db8388869861c50b9a9414f7ba7c7fea
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-22 21:40:18 +0300

    feat(contract): export template introspection contracts

--- packages/nodejs/codepotx/src/contract/index.ts
+++ packages/nodejs/codepotx/src/contract/index.ts
@@ -7,6 +7,8 @@ export type * from './artifact.types';
 export type * from './authoring-artifact.types';
 export type * from './generation-artifact.types';
 export type * from './template-artifact.types';
+export type * from './template-variables.types';
+export type * from './template-introspection.types';
 export type * from './common.types';
 export type * from './diagnostics.types';
 export type * from './events.types';
