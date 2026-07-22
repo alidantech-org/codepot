b1f069529282c7f3e4ff66324b8d14bbf9cbafd8
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-22 22:10:39 +0300

    feat(generation): export production planning utilities

--- packages/nodejs/codepotx/src/generation/index.ts
+++ packages/nodejs/codepotx/src/generation/index.ts
@@ -1,4 +1,24 @@
 export { compileCodepotFile, findTask } from './codepot-file';
+export { executePlannedCommands, taskCommands } from './command-execution';
 export { createGenerationEngine, DefaultGenerationEngine } from './generation-engine';
-export { artifactReference, joinPath, planClean, planCommands, planFiles } from './planning';
+export { createRelativeImportAdapter, RelativeImportAdapter } from './imports';
+export type * from './imports.types';
+export {
+  buildGenerationManifest,
+  currentFileDigest,
+  loadGenerationManifest,
+  manifestPath,
+  staleManagedFiles,
+  writeGenerationManifest,
+} from './manifest';
+export { applyManagedWrite, ManagedWriteError } from './managed-write';
+export {
+  artifactReference,
+  joinPath,
+  planClean,
+  planCommands,
+  planFiles,
+} from './planning';
+export { countGenerationFiles, createGenerationReport } from './report';
+export { GenerationFileTransaction } from './transaction';
 export type * from './generation.types';
