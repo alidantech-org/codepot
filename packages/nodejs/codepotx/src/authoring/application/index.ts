22ff4a1597eaa0c2a2196d2d0620ab6f486fac19
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:09:18 +0300

    refactor(authoring): add application ownership barrel

---
+++ packages/nodejs/codepotx/src/authoring/application/index.ts
@@ -0,0 +1,5 @@
+export { cacheAuthoring } from './cache-authoring';
+export { compileAuthoring } from './compile-authoring';
+export { inspectAuthoring } from './inspect-authoring';
+export { loadAuthoringArtifact } from './load-authoring-artifact';
+export { validateAuthoring } from './validate-authoring';
