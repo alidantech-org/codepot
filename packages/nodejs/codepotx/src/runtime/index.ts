3a990146427efffee82799f23e46730605254ec5
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 15:06:31 +0300

    refactor(runtime): curate runtime public exports

--- packages/nodejs/codepotx/src/runtime/index.ts
+++ packages/nodejs/codepotx/src/runtime/index.ts
@@ -1,7 +1,9 @@
 export { createCodepotRuntime } from './create-runtime';
-export { composeDefaultCodepotRuntime, createDefaultCodepotRuntime } from './default-runtime';
+export {
+  composeDefaultCodepotRuntime,
+  createDefaultCodepotRuntime,
+} from './composition/index';
 export { CodepotRuntime } from './runtime';
-export { RunContextStore } from './run-context';
-
+export { RunContextStore } from './context/index';
 export type * from './default-runtime.types';
 export type { RuntimeDependencies } from './runtime-dependencies.types';
