3f2a09f75a2041434a9c7c3b2133f5a5f74090ee
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 15:02:25 +0300

    refactor(platform): add memory adapter ownership barrel

---
+++ packages/nodejs/codepotx/src/platform/memory/index.ts
@@ -0,0 +1,6 @@
+export { MemoryCache } from './cache';
+export { MemoryCommandRunner } from './command-runner';
+export type { MemoryCommandHandler } from './command-runner';
+export { MemoryFileSystem } from './file-system';
+export { MemoryModuleLoader } from './module-loader';
+export { MemorySourceRegistry } from './sources';
