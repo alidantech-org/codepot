97883c1779617ee960779fd713258a5ff9ff0842
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:10:18 +0300

    refactor(templating): add config ownership barrel

---
+++ packages/nodejs/codepotx/src/templating/config/index.ts
@@ -0,0 +1,11 @@
+export { normalizePathsConfig } from './normalized-paths-config';
+export type {
+  NormalizedPathsConfig,
+  NormalizedWritePolicyInput,
+} from './normalized-paths-config';
+export type {
+  PathsFileInput,
+  PathsFolderInput,
+  TemplateVariableRequirementInput,
+} from './paths-input.types';
+export { validatePathsConfig } from './validate-paths-config';
