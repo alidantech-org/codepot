619a83e369656cad07ececfa05852833c02b3bbb
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:10:45 +0300

    refactor(templating): add context ownership barrel

---
+++ packages/nodejs/codepotx/src/templating/context/index.ts
@@ -0,0 +1,6 @@
+export { createCompiledTemplateContext } from './create-template-context';
+export {
+  buildTemplateContext,
+  createNameSet,
+} from '../template-context';
+export type { TemplateNameSet } from '../template-context';
