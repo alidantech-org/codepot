fac15eedfc35b246d00d8a17bbea3c4a5fa8a0fe
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 11:51:23 +0300

    refactor(contract): switch public facade to owned modules (#15)

--- packages/nodejs/codepotx/src/contract/index.ts
+++ packages/nodejs/codepotx/src/contract/index.ts
@@ -1,18 +1,12 @@
 export {
   CODEPOT_ARTIFACT_VERSION,
   CODEPOT_PROTOCOL_VERSION,
-} from './artifact.types';
+} from './protocol/index';
 
-export type * from './artifact.types';
-export type * from './authoring-artifact.types';
-export type * from './generation-artifact.types';
-export type * from './template-artifact.types';
-export type * from './template-variables.types';
-export type * from './template-introspection.types';
-export type * from './common.types';
-export type * from './diagnostics.types';
-export type * from './events.types';
-export type * from './ports.types';
-export type * from './requests.types';
-export type * from './runtime.types';
-export type * from './sources.types';
+export type * from './protocol/index';
+export type * from './sources/index';
+export type * from './diagnostics/index';
+export type * from './artifacts/index';
+export type * from './operations/index';
+export type * from './events/index';
+export type * from './ports/index';
