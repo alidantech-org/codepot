2af343994ec590490f32a6471256ceb9e77404ec
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:12:31 +0300

    refactor(generation): add manifest ownership barrel

---
+++ packages/nodejs/codepotx/src/generation/manifests/index.ts
@@ -0,0 +1,9 @@
+export {
+  buildGenerationManifest,
+  currentFileDigest,
+  loadGenerationManifest,
+  manifestPath,
+  staleManagedFiles,
+  writeGenerationManifest,
+} from '../manifest';
+export type { ManifestDependencies } from '../manifest';
