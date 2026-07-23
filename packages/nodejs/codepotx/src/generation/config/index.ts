f12cdbf4f2b1d9847cf48668ccc94dd67e23ec63
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:11:55 +0300

    refactor(generation): add config ownership barrel

---
+++ packages/nodejs/codepotx/src/generation/config/index.ts
@@ -0,0 +1,10 @@
+export {
+  compileCodepotFile,
+  findTask,
+} from '../codepot-file';
+export type {
+  CodepotCommandInput,
+  CodepotFileInput,
+  CodepotTaskInput,
+  SourceInput,
+} from '../generation.types';
