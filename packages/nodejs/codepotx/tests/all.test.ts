58e81cbc10d726efbeec8bf29cf9b3645b6ac819
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 15:37:55 +0300

    test(structure): include platform unit group

--- packages/nodejs/codepotx/tests/all.test.ts
+++ packages/nodejs/codepotx/tests/all.test.ts
@@ -3,6 +3,7 @@ import './compatibility/index.test';
 import './contract/index.test';
 import './unit/authoring/index.test';
 import './unit/generation/index.test';
+import './unit/platform/index.test';
 import './unit/runtime/index.test';
 import './unit/templating/index.test';
 import './integration/index.test';
