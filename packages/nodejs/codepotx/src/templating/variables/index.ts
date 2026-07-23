980fbd957aca6bf98484b528600a2d27f732e904
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:11:36 +0300

    refactor(templating): add variables ownership barrel

---
+++ packages/nodejs/codepotx/src/templating/variables/index.ts
@@ -0,0 +1,7 @@
+export { listTemplateVariables } from './list-template-variables';
+export {
+  buildTemplateVariableCatalog,
+  formatTemplateVariableCatalog,
+  validateTemplateContext,
+} from '../template-variables';
+export { validateCompiledTemplateContext } from './validate-template-context';
