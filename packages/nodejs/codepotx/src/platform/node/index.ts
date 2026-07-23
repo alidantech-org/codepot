2a3c3b312b86954197973cc3938ff2a20faaeed1
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 15:02:16 +0300

    refactor(platform): add node adapter ownership barrel

---
+++ packages/nodejs/codepotx/src/platform/node/index.ts
@@ -0,0 +1,5 @@
+export { NodeCommandRunner } from './command-runner';
+export { NodeFileSystem } from './file-system';
+export { FileSystemCache } from './file-system-cache';
+export { TsxModuleLoader } from './module-loader';
+export { DefaultSourceResolver } from './source-resolver';
