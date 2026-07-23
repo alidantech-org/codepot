321daf23b3b9d2c14a0f4606e00cc7de944531bf
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:11:13 +0300

    refactor(templating): add paths ownership barrel

---
+++ packages/nodejs/codepotx/src/templating/paths/index.ts
@@ -0,0 +1,14 @@
+export {
+  compilePathParts,
+  compilePathTokens,
+  resolveExpression,
+  resolveOutputTokens,
+} from '../path-tokens';
+export {
+  isPartialPath,
+  joinTemplatePath,
+  matchesTemplateGlob,
+  partialNameFor,
+  relativeTemplatePath,
+  unsafeTemplatePath,
+} from './template-paths';
