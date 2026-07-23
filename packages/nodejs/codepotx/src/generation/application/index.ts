b679070279927b6d914613f177bde7e9b6304b50
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:11:47 +0300

    refactor(generation): add application ownership barrel

---
+++ packages/nodejs/codepotx/src/generation/application/index.ts
@@ -0,0 +1,7 @@
+export { cleanGeneration } from './clean-generation';
+export { executeGeneration } from './execute-generation';
+export { loadCodepotFile } from './load-codepot-file';
+export { planGeneration } from './plan-generation';
+export { renderGeneration } from './render-generation';
+export { runGenerationCommands } from './run-generation-commands';
+export { writeGeneration } from './write-generation';
