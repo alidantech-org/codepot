25d1c96c4f2794284ae277eb16f0b1d6b8897f1c
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:50:10 +0300

    refactor(runtime): add dispatch ownership barrel

---
+++ packages/nodejs/codepotx/src/runtime/dispatch/index.ts
@@ -0,0 +1,7 @@
+export { createRuntimeOperationHandlers } from './create-runtime-handlers';
+export { dispatchRuntimeOperation } from './dispatch-runtime-operation';
+export { selectRuntimeFeatures } from './select-runtime-features';
+export type {
+  RuntimeOperationHandler,
+  RuntimeOperationHandlerRegistry,
+} from './runtime-handler.types';
