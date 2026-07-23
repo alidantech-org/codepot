2ad7d6202f92233d2e38172b0c349f3a5e2701b5
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:10:29 +0300

    refactor(templating): add compiler ownership barrel

---
+++ packages/nodejs/codepotx/src/templating/compiler/index.ts
@@ -0,0 +1,5 @@
+export { assembleTemplatePack } from './assemble-template-pack';
+export { compileTemplateDescriptors } from './compile-template-descriptors';
+export type { CompiledTemplateDescriptorsResult } from './compile-template-descriptors';
+export { discoverTemplateFiles } from './discover-template-files';
+export { validateCompiledTemplatePack } from './validate-template-pack';
