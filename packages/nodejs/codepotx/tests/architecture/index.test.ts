f03bbeeefcaa03edf111f0d1099f4dc915520202
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 15:11:53 +0300

    test(structure): group architecture suites

---
+++ packages/nodejs/codepotx/tests/architecture/index.test.ts
@@ -0,0 +1,4 @@
+import '../..//architecture.test';
+import '../..//contract-partitioning.test';
+import '../..//runtime-platform-modularization.test';
+import '../..//structural-modularization.test';
