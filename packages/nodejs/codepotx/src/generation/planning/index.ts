f53210f6019b0ce72ca0f9f1a8e428bae8e3131a
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-23 14:12:05 +0300

    refactor(generation): add planning ownership barrel

---
+++ packages/nodejs/codepotx/src/generation/planning/index.ts
@@ -0,0 +1,15 @@
+export {
+  artifactReference,
+  joinPath,
+  planClean,
+  planCommands,
+  planFiles,
+} from '../planning';
+export {
+  evaluateSelection,
+  evaluateSelectionValue,
+} from '../selection-planning';
+export {
+  prepareGenerationPlan,
+} from './prepare-generation-plan';
+export type { PreparedGenerationPlan } from './prepare-generation-plan';
