ae8781dcecc355b5daded34efc7a49484641a07b
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-27 20:03:03 +0300

    feat: export planning and generation APIs

--- packages/python/codepotg-v2/src/codepotg/generation/__init__.py
+++ packages/python/codepotg-v2/src/codepotg/generation/__init__.py
@@ -6,10 +6,42 @@ from codepotg.domain.generation import (
     SelectorRegistry,
 )
 
+from .context import RenderContextBuilder
+from .discovery import PackDiscoveryError, discover_pack_files
+from .expressions import ExpressionError, evaluate_text, resolve_expression
+from .models import (
+    ArtifactPlan,
+    DiscoveredPackFile,
+    GeneratedArtifact,
+    GenerationData,
+    GenerationPlan,
+    MemoryOutput,
+    ModuleCollection,
+    ModuleDescriptor,
+    PackFileKind,
+)
+from .planner import ProjectPlanner
+
 __all__ = [
+    "ArtifactPlan",
     "DEFAULT_SELECTOR_REGISTRY",
+    "DiscoveredPackFile",
+    "ExpressionError",
+    "GeneratedArtifact",
+    "GenerationData",
+    "GenerationPlan",
+    "MemoryOutput",
+    "ModuleCollection",
+    "ModuleDescriptor",
+    "PackDiscoveryError",
+    "PackFileKind",
+    "ProjectPlanner",
+    "RenderContextBuilder",
     "SelectionCardinality",
     "SelectionContext",
     "SelectorDescriptor",
     "SelectorRegistry",
+    "discover_pack_files",
+    "evaluate_text",
+    "resolve_expression",
 ]
