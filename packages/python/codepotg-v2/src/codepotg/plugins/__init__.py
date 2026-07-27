68af560e1a044565c0a8ff8126bb1d281a8555f9
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-27 08:49:05 +0300

    refactor(codepotg-v2): publish organized plugin namespace

---
+++ packages/python/codepotg-v2/src/codepotg/plugins/__init__.py
@@ -0,0 +1,9 @@
+from .descriptors import PluginCategory, PluginDescriptor, PluginTrust
+from .registry import PluginRegistry
+
+__all__ = [
+    "PluginCategory",
+    "PluginDescriptor",
+    "PluginRegistry",
+    "PluginTrust",
+]
