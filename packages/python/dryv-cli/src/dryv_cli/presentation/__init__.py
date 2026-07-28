9cd617b5644d718b3ffb40a57b58b6627b4dd34f
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-07-28 16:00:50 +0300

    feat(dryv-cli): publish presentation helpers

---
+++ packages/python/dryv-cli/src/dryv_cli/presentation/__init__.py
@@ -0,0 +1,18 @@
+from .console import activity, get_console
+from .results import (
+    render_cancelled,
+    render_failure,
+    render_json,
+    render_operation,
+    render_runtime,
+)
+
+__all__ = [
+    "activity",
+    "get_console",
+    "render_cancelled",
+    "render_failure",
+    "render_json",
+    "render_operation",
+    "render_runtime",
+]
