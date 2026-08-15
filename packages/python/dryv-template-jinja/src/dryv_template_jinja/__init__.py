ff5fc9ebac899488d484853ce5de89fa4a6bce65
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-08-16 02:49:02 +0300

    feat(jinja): expose render session client

--- packages/python/dryv-template-jinja/src/dryv_template_jinja/__init__.py
+++ packages/python/dryv-template-jinja/src/dryv_template_jinja/__init__.py
@@ -8,15 +8,33 @@ from .engine import (
 from .helpers import HelperDescriptor, HelperKind
 from .plugin import create_plugin
 from .rules import JinjaEngineRules
+from .session import (
+    CONTEXT_VERSION,
+    RENDERER_CAPABILITY,
+    RENDER_PROTOCOL_VERSION,
+    JinjaRenderSession,
+    RenderDiagnosticMessage,
+    RenderResultMessage,
+    RenderedOutputMessage,
+    RendererHelloMessage,
+)
 
 __all__ = [
     "CAPABILITIES",
+    "CONTEXT_VERSION",
     "ENGINE_BEHAVIOR_VERSION",
     "HelperDescriptor",
     "HelperKind",
     "JinjaEngineRules",
+    "JinjaRenderSession",
     "JinjaTemplateEngine",
     "PACKAGE_VERSION",
+    "RENDERER_CAPABILITY",
+    "RENDER_PROTOCOL_VERSION",
+    "RenderDiagnosticMessage",
+    "RenderResultMessage",
+    "RenderedOutputMessage",
+    "RendererHelloMessage",
     "SUFFIXES",
     "create_plugin",
 ]
