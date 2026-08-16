21b8cf3b3907a10c6fdaee512512b09fc862a9fa
PETER IRUNGU <117439552+Alidantech@users.noreply.github.com>
2026-08-16 06:55:49 +0300

    feat(dryv-api): expose Render Client protocol boundary

--- packages/python/dryv-api/src/dryv_api/renderers/__init__.py
+++ packages/python/dryv-api/src/dryv_api/renderers/__init__.py
@@ -1,1 +1,58 @@
 """External Render Client protocol, registry, preflight and scheduling."""
+
+from .connection import RendererConnection, RendererConnectionState
+from .preflight import (
+    JobPreflight,
+    PreflightCoordinator,
+    PreflightKey,
+    PreflightReport,
+    TemplatePreflightError,
+)
+from .protocol import (
+    RENDER_PROTOCOL_VERSION,
+    ArtifactBegin,
+    ArtifactChunk,
+    ArtifactEnd,
+    CancelRender,
+    ContextContractPayload,
+    ContextPayload,
+    PlannedOutputPayload,
+    RenderClientTransport,
+    RenderComplete,
+    RenderFailed,
+    RendererDiagnostic,
+    RendererHello,
+    RenderRequest,
+    TemplatePayload,
+    ValidateTemplateRequest,
+    ValidateTemplateResult,
+)
+from .registry import RendererRegistry
+
+__all__ = [
+    "ArtifactBegin",
+    "ArtifactChunk",
+    "ArtifactEnd",
+    "CancelRender",
+    "ContextContractPayload",
+    "ContextPayload",
+    "JobPreflight",
+    "PlannedOutputPayload",
+    "PreflightCoordinator",
+    "PreflightKey",
+    "PreflightReport",
+    "RENDER_PROTOCOL_VERSION",
+    "RenderClientTransport",
+    "RenderComplete",
+    "RenderFailed",
+    "RenderRequest",
+    "RendererConnection",
+    "RendererConnectionState",
+    "RendererDiagnostic",
+    "RendererHello",
+    "RendererRegistry",
+    "TemplatePayload",
+    "TemplatePreflightError",
+    "ValidateTemplateRequest",
+    "ValidateTemplateResult",
+]
